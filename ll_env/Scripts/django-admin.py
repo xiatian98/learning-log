2828bcb43de062beb6de6ff323ec1007fbf2365a
qinhuong <qinhuong@outlook.com>
2022-12-22 21:45:57 +0800

    删除idea和ll_env

--- ll_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!d:\python project file\study_2021\python入门到实践\python_projects\learning_log\ll_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
